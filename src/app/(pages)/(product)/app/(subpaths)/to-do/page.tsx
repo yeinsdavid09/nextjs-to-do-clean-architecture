125d660e5361bd3d51cdda7529570fc52352a083
Yeins David Llano Hernandez <yeinsdavid09@gmail.com>
2025-08-09 22:17:00 -0500

    feat(to-do): create initial module pages

---
+++ src/app/(pages)/(product)/app/(subpaths)/to-do/page.tsx
@@ -0,0 +1,11 @@
+export default function ToDoPage() {
+  //#region --------------------------------- Return ---------------------------------
+
+  return (
+    <section>
+      <p>To Do Page</p>
+    </section>
+  );
+
+  //#endregion
+}
